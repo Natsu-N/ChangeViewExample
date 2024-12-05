e2f63fec18f24fe18a339d92eef639af2927fca2
Ostashenko Anastasiya <ostashenko.anastasiya2001@gmail.com>
2024-12-05 10:24:11 +0300

    Initial project

---
+++ ChangeViewExample.ViewModel/MainView.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace ChangeViewExample.ViewModel
+{
+    public class MainView
+    {
+        [Reactive]
+        public string Text { get; set; } = "Initial with View Model project";
+    }
+}
---
+++ ChangeViewExample/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ChangeViewExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
